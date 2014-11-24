882ca29bc2a96795e297f7ad7ffb29c18954e8dc
Tony Vu <vuductrung2003@gmail.com>
2014-11-24 04:04:14 -0500

    pearl

--- wp-content/themes/twentythirteen/header.php
+++ wp-content/themes/twentythirteen/header.php
@@ -21,6 +21,8 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
+	<!-- Google Shopping Campaign -->
+	<meta name="google-site-verification" content="eGKTJgXwkwmFYP1paiGM2b-GgLbDIcs8a32jDwW7lKU" />
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
